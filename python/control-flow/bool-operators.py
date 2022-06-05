77e11d2f7d7b55d97bb409b5bc99512fe6a03b1c
Nicole <nzolnier@gmail.com>
2022-06-05 13:25:18 -0400

    add: control flow

---
+++ python/control-flow/bool-operators.py
@@ -0,0 +1,22 @@
+statement_one = (2 + 2 + 2 >= 6) and (-1 * -1 < 0)
+
+statement_two = (4*2 <= 8) and (7-1 == 6)
+
+print(statement_one)
+print(statement_two)
+
+statement_one = (2 - 1 > 3) or (-5 * 2 == -10)
+
+statement_two = (9 + 5 <= 15) or (7 != 4 + 3)
+
+print(statement_one)
+print(statement_two)
+
+
+statement_one = not (1 + 1 == 2)
+
+statement_two = not (2 + 2 == 5)
+
+print(statement_one)
+print(statement_two)
+
---
+++ python/control-flow/ifs.py
@@ -0,0 +1,41 @@
+user_name = "Dave"
+
+if user_name == "Dave":
+    print("get off, Dave")
+if user_name == "angela_catlady_87":
+    print("i know it's you, Dave, get off!")
+
+
+x = 20
+y = 20
+
+if x == y:
+    print("these numbers are the same")
+
+credits = 120
+
+if credits >= 120:
+    print("you have enough credits to graduate")
+if not credits >= 120:
+    print("you do not have enough credits to graduate")
+
+
+age = 20
+if age >= 13:
+    print("you can watch the movie")
+else:
+    print("you can't watch the movie")
+
+
+grade = 100
+
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+elif grade >= 60:
+    print("D")
+else:
+    print("F")
