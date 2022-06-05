916a169c3788be154ec2e399daa0d5befcc04c33
Nicole <nzolnier@gmail.com>
2022-06-05 16:05:14 -0400

    add: lists

---
+++ python/lists/2d-list.py
@@ -0,0 +1,17 @@
+heights = [["jenny", 61], ["alexus", 70], ["sam", 67]]
+
+jennys_height = heights[0][1]
+
+print(jennys_height)
+
+incoming_class = [["kenny", "american", 9], ["tanya", "russian", 9], ["madison", "indian", 7]]
+
+print(incoming_class)
+
+# modifying 2d lists
+incoming_class[2][2] = 8
+print(incoming_class)
+
+incoming_class[-3][-3] = "ken"
+
+print(incoming_class)
